3f7f76d9b7a01b6b1f44eebeceb371d8784eaf92
otabek-kholmirzaev <kholmirzaevotabek1@gmail.com>
2025-12-10 01:26:51 +0500

    Add 'delete_book' delete endpoint

--- books.py
+++ books.py
@@ -41,3 +41,10 @@ async def update_book(updated_book=Body()):
     for i in range(len(BOOKS)):
         if BOOKS[i]['title'].casefold() == updated_book['title'].casefold():
             BOOKS[i] = updated_book
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i]['title'].casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
