7301f1ef2bbc6b23cbcde4b84fb5fede6ba3d7a6
otabek-kholmirzaev <kholmirzaevotabek1@gmail.com>
2025-12-11 23:35:32 +0500

    Add create_book endpoint v2

--- books2.py
+++ books2.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -16,6 +18,13 @@ class Book:
         self.description = description
         self.rating = rating
 
+class BookRequest(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
     Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
@@ -29,3 +38,12 @@ BOOKS = [
 async def read_all_books():
     return BOOKS
 
+@app.post("/create_book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.dict())
+    print(type(new_book))
+    BOOKS.append(find_book_id(new_book))
+
+def find_book_id(book: Book):
+    book.id = BOOKS[-1].id + 1 if len(BOOKS) > 0 else 1
+    return book
